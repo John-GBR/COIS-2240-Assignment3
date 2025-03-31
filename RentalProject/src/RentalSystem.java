4e3938b62748f43b467fc642b804e7c7712e889d
John Cooney <jrc9495@gmail.com>
2025-03-31 19:20:19 -0400

    Subtask 2: implemented the saveVehicle, saveCustomer & saveRecord methods to RentalSystem, preserving the respective info when closing the program. Also modified the Vehicle class' getInfo method to remove an unecissary

--- RentalProject/src/RentalSystem.java
+++ RentalProject/src/RentalSystem.java
@@ -1,6 +1,9 @@
 import java.util.List;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RentalSystem {
     private List<Vehicle> vehicles = new ArrayList<>();
@@ -15,21 +18,90 @@ public class RentalSystem {
     {
     	if (instance == null)
     		instance = new RentalSystem();
+    		
     	return instance;
+    }
+    
+    // Adds vehicle details to vehicles.txt. Called inside addVehicle()
+    private void saveVehicle(Vehicle vehicle)
+    {
+    	try {
+    		// Creates a BufferedWriter object
+			BufferedWriter writer = new BufferedWriter(new FileWriter("vehicles.txt", true));
+			
+			// String that holds the output of the vehicle's getInfo() method, plus the vehicle's type
+			String vehicleInfo = vehicle.getInfo();
+			
+			// Adds to the beginning of the vehicleInfo string the type of vehicle
+			if (vehicle instanceof SportCar)
+				vehicleInfo = "SportCar" + vehicleInfo;
+			else if (vehicle instanceof Car)
+				vehicleInfo = "Car" + vehicleInfo;
+			else if (vehicle instanceof Motorcycle)
+				vehicleInfo = "Motorcycle" + vehicleInfo;
+			else if (vehicle instanceof Truck)
+				vehicleInfo = "Truck" + vehicleInfo;
+
+			// Writes the vehicleInfo to the file then closes the writer
+			writer.write(vehicleInfo + "\n");
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    // Adds customer details to customers.txt. Called inside addCustomer()
+    private void saveCustomer(Customer customer)
+    {
+    	try {
+    		// Creates a BufferedWriter object
+			BufferedWriter writer = new BufferedWriter(new FileWriter("customers.txt", true));
+
+			// Writes the customer's getInfo() output to the file then closes the writer
+			writer.write(customer.toString() + "\n");
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    // Adds rental record details to rental_records.txt. Called at the end of rentVehicle()
+    //		and returnVehicle() after a record is added to the rental history
+    private void saveRecord(RentalRecord record)
+    {
+    	try {
+    		// Creates a BufferedWriter object
+			BufferedWriter writer = new BufferedWriter(new FileWriter("rental_records.txt", true));
+
+			// Writes the record's getInfo() output to the file then closes the writer
+			writer.write(record.toString() + "\n");
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
+        saveVehicle(vehicle);
     }
 
     public void addCustomer(Customer customer) {
         customers.add(customer);
+        saveCustomer(customer);
     }
 
     public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.AVAILABLE) {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
-            rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, amount, "RENT"));
+            
+            RentalRecord rentalRecord = new RentalRecord(vehicle, customer, date, amount, "RENT");
+            rentalHistory.addRecord(rentalRecord);
+            saveRecord(rentalRecord);
+            
             System.out.println("Vehicle rented to " + customer.getCustomerName());
         }
         else {
@@ -40,7 +112,11 @@ public class RentalSystem {
     public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.RENTED) {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
-            rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, extraFees, "RETURN"));
+            
+            RentalRecord rentalRecord = new RentalRecord(vehicle, customer, date, extraFees, "RETURN");
+            rentalHistory.addRecord(rentalRecord);
+            saveRecord(rentalRecord);
+            
             System.out.println("Vehicle returned by " + customer.getCustomerName());
         }
         else {
