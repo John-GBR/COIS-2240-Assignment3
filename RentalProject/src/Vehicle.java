79e8ca1e07735f48d39955b8b174aba0b38a38ab
John Cooney <jrc9495@gmail.com>
2025-04-01 22:49:24 -0400

    Subtask 5: refactored the Vehicle class' constructor to use a helper method to properly capitalize make & model instead of using repeated logic.

--- RentalProject/src/Vehicle.java
+++ RentalProject/src/Vehicle.java
@@ -7,21 +7,29 @@ public abstract class Vehicle {
 
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
+    // 3 parameter constructor
     public Vehicle(String make, String model, int year) {
+    	// Checks if the given make & model are null / empty, if not assign them after properly capitalizing them using capitalize()
     	if (make == null || make.isEmpty())
     		this.make = null;
     	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
+    		this.make = capitalize(make);
     	
     	if (model == null || model.isEmpty())
     		this.model = null;
     	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
+    		this.model = capitalize(model);
     	
+    	// Assigns the year, sets license plate to null & sets the vehicle to AVAILIBLE
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
     }
+
+    // Helper method that returns the given string with only it's 1st letter capitalized
+	private String capitalize(String input) {
+		return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+	}
 
     public Vehicle() {
         this(null, null, 0);
@@ -48,5 +56,4 @@ public abstract class Vehicle {
     public String getInfo() {
         return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status;
     }
-
 }
